5b950a08d3d1a621301cac69aeab982d51c04a56
unknown <nikinitinshaji@gmail.com>
2021-08-03 15:47:37 +0400

    Final Edits

--- static/js/selectcrop.js
+++ static/js/selectcrop.js
@@ -1,26 +1,7 @@
 const items = document.getElementsByClassName('item');
 const input = document.getElementById('cat-input')
 
-month=document.getElementById('month-list');
-month.addEventListner('click',limit );
-function limit()
-{    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-    var yyyy = today.getFullYear();
-    var nextyear=today.getFullYear()+1;
- if(dd<10){
-        dd='0'+dd
-    } 
-    if(mm<10){
-        mm='0'+mm
-    } 
 
-today = yyyy+'-'+mm+'-'+dd;
-next=nextyear+'-'+mm+'-31'
-document.getElementById('month-list').min=today;
-document.getElementById('month-list').max=next;
-}
 
 for (let i = 0; i < items.length; i++) {
     items[i].addEventListener('click', () => {
